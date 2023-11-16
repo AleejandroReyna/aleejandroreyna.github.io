cbff3aa8d28b1078fa8901539372822160affc82
Alejandro Reyna <AleejandroReyna@gmail.com>
2023-11-15 23:15:13 -0600

    testing: Added libraries for testing dom
    And added first component test

--- src/__test__/screens/HomeScreen/HomeScreen.test.tsx
+++ src/__test__/screens/HomeScreen/HomeScreen.test.tsx
@@ -1,5 +1,16 @@
-describe('HomeScreen', () => {
-    test('demo', () => {
-        expect(true).toBe(true)
+// Dependencies
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+
+// Component
+import { HomeScreen } from '../../../screens'
+
+// Tests
+describe('<HomeScreen />', () => {
+    describe('Content', () => {
+        test('Should render main content', () => {
+            const { getByText } = render(<HomeScreen />)
+            expect(getByText('Home Screen')).toBeTruthy()
+        })
     })
 })
