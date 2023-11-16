98a315730c80d5aa7cf1f02bb6bdb36071e3eacb
Alejandro Reyna <AleejandroReyna@gmail.com>
2023-11-16 00:51:52 -0600

    coverage: Added threshold

--- jest.config.ts
+++ jest.config.ts
@@ -10,4 +10,12 @@ export default {
     moduleNameMapper: {
         '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
     },
+    // Threshold
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+        },
+    },
 }
