b3ffc01a20f7bffbef45aacfc577f4491044e96e
Alejandro Reyna <AleejandroReyna@gmail.com>
2025-03-31 20:21:08 -0600

    home: Added portfolio section

--- src/screens/Home/Portfolio/_Portfolio.tsx
+++ src/screens/Home/Portfolio/_Portfolio.tsx
@@ -1,12 +1,106 @@
+import { 
+  SiReact,
+  SiTypescript,
+  SiWordpress,
+  SiMobx,
+  SiJavascript,
+  SiVuedotjs,
+  SiPhp,
+  SiLaravel,
+  SiPython,
+  SiDjango,
+  SiNodedotjs,
+  SiExpress,
+  SiMessenger,
+  SiTwilio,
+  SiBitrise,
+  SiAwsfargate
+} from "@icons-pack/react-simple-icons";
 export const Portfolio = () => {
   return (
     <section className="bg-white py-24">
-    <div className="mx-auto max-w-md bg-white md:max-w-7xl lg:max-w-7xl">
-      <h2 className="text-center text-5xl font-bold py-4">Porfolio</h2>
-      <p className="text-2xl text-center">
-        Here are some of the projects I have worked on. I am always looking for new challenges and opportunities to learn and grow.
-      </p>
+      <div className="mx-auto max-w-md bg-white md:max-w-7xl lg:max-w-7xl">
+        <h2 className="text-center text-5xl font-bold py-4">Porfolio</h2>
+        <p className="text-2xl text-center">
+          Here are some of the projects I have worked on. I am always looking for new challenges and opportunities to learn and grow.
+        </p>
+
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 py-16">
+
+          <article className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Zigi App</h2>
+              <p>Fintech app to democratize the banking.</p>
+              <strong>Stack: </strong>
+              <div className="flex">
+                <SiReact className="mr-2" />
+                <SiTypescript className="mr-2" />
+                <SiMobx className="mr-2" />
+                <SiBitrise className="mr-2" />
+                <SiWordpress />
+              </div>
+              <div className="card-actions">
+                <button className="btn btn-neutral">More about</button>
+              </div>
+            </div>
+          </article>
+
+          <article className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">XP3 Talent</h2>
+              <p>Human resources web app</p>
+              <strong>Stack: </strong>
+              <div className="flex">
+                <SiPhp className="mr-2" />
+                <SiLaravel className="mr-2" />
+                <SiJavascript className="mr-2" />
+                <SiVuedotjs className="mr-2" />
+                <SiAwsfargate />
+              </div>
+              <div className="card-actions">
+                <button className="btn btn-neutral">More about</button>
+              </div>
+            </div>
+          </article>
+
+          <article className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Afinidata</h2>
+              <p>Chatbot for first childhood development</p>
+              <strong>Stack: </strong>
+              <div className="flex">
+                <SiPython className="mr-2" />
+                <SiDjango className="mr-2" />
+                <SiNodedotjs className="mr-2" />
+                <SiExpress className="mr-2" />
+                <SiJavascript className="mr-2" />
+                <SiReact className="mr-2" />
+                <SiVuedotjs className="mr-2" />
+                <SiMessenger className="mr-2" />
+                <SiTwilio className="mr-2" />
+              </div>
+              <div className="card-actions">
+                <button className="btn btn-neutral">More about</button>
+              </div>
+            </div>
+          </article>
+        </div>
       </div>
-      </section>
+    </section>
   );
 }
