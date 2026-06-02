89fe05d2fbe605d3f6b17d76b4e4f9f70481bff1
Alejandro Reyna <me@alejandroreyna.com>
2026-06-01 23:33:01 -0600

    gcp-migration: Added artifact registry
    And artifact registry repository

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,17 @@
-# -----------------------------------------------------------------
-# 🔌 HABILITAR LA API DE CLOUD RUN
-# -----------------------------------------------------------------
-# Le pedimos a Google Cloud que active la API de Cloud Run en nuestro proyecto.
-# Esto es indispensable antes de poder crear cualquier servicio de Cloud Run.
-
 resource "google_project_service" "cloud_run_api" {
   service            = "run.googleapis.com"
   disable_on_destroy = false
+}
+
+resource "google_project_service" "artifact_registry_api" {
+  service            = "artifactregistry.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_artifact_registry_repository" "site_repo" {
+  depends_on    = [google_project_service.artifact_registry_api]
+  location      = "us-central1"
+  repository_id = "site-images"
+  description   = "Repositorio Docker para almacenar las imágenes de Next.js/Payload CMS"
+  format        = "DOCKER"
 }
