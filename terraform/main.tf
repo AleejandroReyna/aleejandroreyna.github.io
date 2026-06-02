877f20a802dffd40006ce17683c0ff62c832b5c7
Alejandro Reyna <me@alejandroreyna.com>
2026-06-01 23:57:08 -0600

    gcp-migration: Added cloud run instance
    And attached subdomain

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,3 +15,51 @@ resource "google_artifact_registry_repository" "site_repo" {
   description   = "Repositorio Docker para almacenar las imágenes de Next.js/Payload CMS"
   format        = "DOCKER"
 }
+
+resource "google_cloud_run_v2_service" "nextjs_service" {
+  name     = "alejandro-reyna-site"
+  location = "us-central1"
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 10
+    }
+
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello:latest"
+      ports {
+        container_port = 8080
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      client,
+      client_version,
+      template[0].containers[0].image,
+    ]
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_member" "public_access" {
+  name     = google_cloud_run_v2_service.nextjs_service.name
+  location = google_cloud_run_v2_service.nextjs_service.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+resource "google_cloud_run_domain_mapping" "portfolio_domain" {
+  location = "us-central1"
+  name     = "portfolio.alejandroreyna.com"
+
+  metadata {
+    namespace = "aleejandroreyna"
+  }
+
+  spec {
+    route_name = google_cloud_run_v2_service.nextjs_service.name
+  }
+}
